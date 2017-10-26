dc5fcafe0d3c70b4d22339bf8cc309357eb9b5b6
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-26 19:51:05 +1100

    Update document controller spec to incorporate devise login

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe DocumentsController, type: :controller do
 
+  before do
+    @company = Company.create! name: 'Test Company', domain: 'company.com'
+    @user = User.create! email: 'glenn@company.com', password: 'example'
+    sign_in :user, @user #admin # sign_in(scope, resource)
+    @document = Document.create! name: 'Test Name', content: 'Test Content', company: @company
+  end
+
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -11,7 +18,7 @@ RSpec.describe DocumentsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, params: { id: @document.id }
       expect(response).to have_http_status(:success)
     end
   end
